c6c77df86f1701d1f905acce246639c676d77cd5
Adil Soomro <adilgulsoomro@gmail.com>
2023-03-22 11:09:23 -0400

    Second Commit

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ states.onchange = function() {
         // Joining a string "state name:" and adding it to the text value of the selected dropdown
     console.log('img/' + states.value + '@2x.jpg');
         // Joining a string "img/" and the state number with "@2xjpg"
-
     document.getElementById('postcard').style.backgroundImage = 'url(img/' + states.value + '@2x.jpg)';
+        // Selects the background image of the postcard ID and sets it to a link by adding 2 strings and the state abbreviation
+    document.getElementById('state-heading').innerHTML = states.options[states.selectedIndex].text;
+        // Selects the innerHTML of the element with the ID of 'state-heading' and assign it the value of the selected state from the dropdown menu
+    document.getElementById('greeting').innerHTML = 'Greetings from:';
+        // Changes the innerHTML of the greeting ID to a simple string
 }
